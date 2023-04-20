da7bfda688ee95669e6e7d251faf168432b776d2
Mark Belanger <mbelanger@olin.edu>
2023-04-20 19:29:59 -0400

    Add use methods for classes in each file

--- character.py
+++ character.py
@@ -7,12 +7,46 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-    pass
+    """
+    Docstring
+    """
+
+    def __init__(self, sprite_path):
+        self._sprite_path = sprite_path
 
 
 class EventCharacter(Character):
-    pass
+    """
+    Docstring
+    """
+
+    def __init__(self, sprite_path, location):
+        """
+        Docstring
+        """
+        super().__init__(sprite_path)
+        self._location = location
 
 
 class PlayerCharacter(Character):
-    pass
+    """
+    Docstring
+    """
+
+    def __init__(self, sprite_path, location):
+        super().__init__(sprite_path)
+        self._location = location
+        self._health = 100 # can be changed later
+
+    @property
+    def health(self):
+        """
+        Docstring
+        """
+        return self._health
+
+    def update_health(self, damage):
+        """
+        Docstring
+        """
+        self._health -= damage
--- controller.py
+++ controller.py
@@ -3,11 +3,32 @@ Take keyboard input to control different scenes within the game.
 """
 
 from abc import ABC, abstractmethod
+import pygame
 
 
 class Controller(ABC):
-    pass
+    """
+    Docstring
+    """
+
+    @abstractmethod
+    def get_next_move(self, options):
+        """
+        Docstring
+        """
 
 
 class TextController(Controller):
-    pass
+    """
+    Docstring
+    """
+
+    def get_next_move(self, options):
+        """
+        Docstring
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN and event.key in options:
+                for key in options:
+                    if key == event.key:
+                        return key
