f929ee29a7f3de49a878ac70b9885c87066d3870
Mark Belanger <mbelanger@olin.edu>
2023-04-22 12:26:36 -0400

    Update drawing_text method

--- drawing_text.py
+++ drawing_text.py
@@ -3,12 +3,12 @@ import pygame
 key_conversion = {
     pygame.K_LEFT: "Left",
     pygame.K_RIGHT: "Right",
-    pygame.K_UP: "Up",
+    pygame.K_UP: "Top",
     pygame.K_DOWN: "Down",
 }
 
 
-def display_travel_directions(next_direction, surface, width, height):
+def display_travel_directions(next_directions, surface, width, height):
     """
     Determines what direction the player can move at
     each decision point in the came
@@ -16,25 +16,23 @@ def display_travel_directions(next_direction, surface, width, height):
     Args:
         next_direction: tuple of integers or None
         surface: pygame surface that the game is being displayed on
-        width: integer representing the width in pixels of the pygame surface
-        height: integer representing the height in pixels of the pygame surface
     """
-    next_direction[0] = pygame.K_LEFT
-    next_direction[1] = pygame.K_RIGHT
-    next_direction[2] = pygame.K_UP
-    next_direction[3] = pygame.K_DOWN
-    directions = []
 
     # Determining what directions are possible at a point
-    for direction, index in enumerate(next_direction):
-        if next_direction[index] != None:
-            directions.append(key_conversion[direction])
+    directions = []
+    if next_directions[0] != None:
+        directions.append("Left")
+    if next_directions[1] != None:
+        directions.append("Right")
+    if next_directions[2] != None:
+        directions.append("Top")
+    if next_directions[3] != None:
+        directions.append("Down")
     directions_str = "or".join(directions)
 
     # Display options within game window
     font = pygame.font.Font("freesansbold.ttf", 20)
     text = font.render(f"Choose {directions_str}", True)
     textRect = text.get_rect()
-    textRect.center = (width // 2, height // 5)
+    textRect.topleft = (width // 2, 4 * height // 5)
     surface.blit(text, textRect)
-    return directions
