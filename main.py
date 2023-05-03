f849de5130aa260d0405826c760724164c990610
Mark Belanger <mbelanger@olin.edu>
2023-05-03 15:37:18 -0400

    Center sprite onto path on map

--- main.py
+++ main.py
@@ -123,7 +123,6 @@ while True:
 
             # Loop will continue with the next result id
             current_event = new_event_id
-            print(current_event)
             FramePerSec.tick(FPS)
 
     except (KeyError, ValueError, IndexError):
